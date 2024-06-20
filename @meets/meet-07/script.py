95f0c175686b9a3b409f3e8b5c8a1ea346476183
Mihail Petrov <mail@mihail-petrov.me>
2024-06-20 19:54:45 +0300

    Update script.py

--- @meets/meet-07/script.py
+++ @meets/meet-07/script.py
@@ -1,68 +1,120 @@
-# Потребителя въвежда в системата абонаментен пакет, който иска да закупи. Пакетите са именовани както следва:
-
-# ENTRY
-# PREMIUM
-# VIP
-
-# Ключ за управление на нашата програма.
+# ========================================================
+# ПЪРВО НИВО НА ПРОВЕРКА - ПАКЕТ
+# ========================================================
 while True:
-
-    print("Изберете абонаментен пакет")
+    print("Избери си пакет")
     print("ENTRY")
     print("PREMIUM")
     print("VIP")
-    customer_plan = input("Избирам пакет: ")
-    is_customer_plan_valid = (  
-                                customer_plan == 'ENTRY'   or 
-                                customer_plan == 'PREMIUM' or
-                                customer_plan == 'VIP'
+    customer_plan = input("избери: ")
+    is_customer_plan_valid = (
+                                customer_plan == "ENTRY" or
+                                customer_plan == "PREMIUM" or
+                                customer_plan == "VIP"
                             )
-    
+
     if is_customer_plan_valid:
+        
+        # ========================================================
+        # ВТОРО НИВО НА ПРОВЕРКА - ИДЕНТИФИКАТОР ЗА ПЕРИОД
+        # ========================================================
 
         while True:
-
-            print("За какъв период искаш да се абонираш")
+            print("Избери си идентификатор за периода")
             print("1. месеци")
             print("2. години")
-            customer_plan_period_id = input("Избирам период: ")
-            is_customer_plan_period_id_valid = (customer_plan_period_id == "1" or
-                                                customer_plan_period_id == "2")
-            
+            plan_period_id          = input("избери: ")
+            is_plan_period_id_valid = (
+                                        plan_period_id == "1" or 
+                                        plan_period_id == "2"
+                                    ) 
 
-            if is_customer_plan_period_id_valid:
+            if is_plan_period_id_valid:
                 
-                while True:
+            # ========================================================
+            # ТРЕТО НИВО НА ПРОВЕРКА - КОЛИЧЕСТВО
+            # ========================================================                
 
+                while True:
+                    plan_period_count = 0
                     is_plan_period_count_valid = False
 
-                    # Месеци
-                    if customer_plan_period_id == "1":
-
-                        print("Изберете броя на месеците, за които искате да се абонирате 1 - 24")
-                        plan_period_count           = int(input("Въведете броя на месеците: "))
+                    # Избрал съм месеци
+                    if plan_period_id == "1":
+                        print("Изберете количество от 1 - 24")
+                        plan_period_count           = int(input("избери: "))
+                        is_plan_period_count_valid  =  (
+                                                        plan_period_count >= 1 and plan_period_count <= 24
+                                                        )
+                    
+                    if plan_period_id == "2":
+                        print("Изберете количество от 1 - 2")
+                        plan_period_count           = int(input("избери: "))
                         is_plan_period_count_valid  = (
-                                                        plan_period_count >= 1 and 
-                                                        plan_period_count <= 24
-                                                    )
-
-                    if customer_plan_period_id == "2":
-
-                        print("Изберете броя на годините, за които искате да се абонирате 1 - 2")
-                        plan_period_count = int(input("Въведете броя на годините: "))
-                        is_plan_period_count_valid = (
-                                                        plan_period_count == 1 or 
+                                                        plan_period_count == 1 or
                                                         plan_period_count == 2
                                                     )
-
+                        
                     if is_plan_period_count_valid:
-                        pass
-                    else:
-                        print("Грешна стойност - моля опитайте отново")
+                        
+                        # ========================================================
+                        # ЧЕТВЪРТО НИВО НА ПРОВЕРКА - НАГРАДА
+                        # ========================================================                
+                        is_applicable_for_price = (
+                                                    (plan_period_id == "1" and plan_period_count == 24) or
+                                                    (plan_period_id == "2" and plan_period_count == 2)
+                        )
 
+                        if is_applicable_for_price:
+
+                            while True:
+                                print("Изберете си награда")
+                                print("1. Телевизор")
+                                print("2. Телефон")
+                                price_id = input("изберете: ")
+                                is_price_id_valid = price_id == "1" or price_id == "2"
+
+                                if is_price_id_valid:
+                                    
+                                    id = ""
+                                    if plan_period_id == "1" and plan_period_count == 1:
+                                        id = "месец"
+                                    elif plan_period_id == "1":
+                                        id = "месеци" 
+                                    elif plan_period_id == "2" and plan_period_count == 1:
+                                        id = "година"
+                                    elif plan_period_id == "2":
+                                        id = "години"
+
+                                    price_name = ""
+                                    if price_id == "1":
+                                        price_name = "ТЕЛЕВИЗОР"
+                                    elif price_id == "2":
+                                        price_name = "ТЕЛЕФОН"
+
+                                    print("Ти избра пакет " + customer_plan + " за период от " + str(plan_period_count) + id + " ти спечели " + price_name )
+
+                                else:
+                                    print("въведе грешна стойност за награда - дай пак")
+
+                        else:
+
+                            id = ""
+                            if plan_period_id == "1" and plan_period_count == 1:
+                                id = "месец"
+                            elif plan_period_id == "1":
+                                id = "месеци" 
+                            elif plan_period_id == "2" and plan_period_count == 1:
+                                id = "година"
+                            elif plan_period_id == "2":
+                                id = "години"
+
+                            print("Ти избра пакет " + customer_plan + " за период от " + str(plan_period_count) + id )
+                    else:
+                        print("грешен период - пробвай пак")
 
             else:
-                print("Грешен избор - моля опитайте отново")
-
+                print("грешна стойност - пробвай пак")
+    
     else:
-        print("Грешен избор - моля опитайте отново")
+        print("грешна стойност - пробвай пак")
