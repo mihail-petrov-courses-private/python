b8af60cc4da0d1b667034869116138d2adb22703
= <mail@mail.bg>
2024-08-08 22:06:07 +0300

    update

--- @meets/meet-14/source/main.py
+++ @meets/meet-14/source/main.py
@@ -1,4 +1,11 @@
 import generate_first
+import generate_second
 
 first_digit = generate_first.generate_digit_first()
+send_digit  = generate_second.generate_digit_second()
+
+print('Първа')
 print(first_digit)
+
+print('Втора')
+print(send_digit)
--- @meets/meet-14/source/util.py
+++ @meets/meet-14/source/util.py
@@ -1,5 +1,21 @@
+MAX_NUMBER_OF_COUNT = 10
+
 # 1. Функция която връща True ако числото, което сме подали като аргумент е четно
-# isEven
+# is_even
+# def is_even(value):
+#     if value % 2 == 0:
+#         return True
+#     return False
+
+def is_even(value):
+    return value % 2 == 0
 
 # 2. Функция която връща True ако числото, което сме подали като аргумент е НЕчетно
-# isOdd
+# is_odd
+# def is_odd(value):
+#     if value % 2 != 0:
+#         return True
+#     return False
+
+def is_odd(value):
+    return value % 2 != 0
