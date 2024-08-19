7e1a53daaecfc2621e210aef54e791e1f3747358
Mihail Petrov <mail@mihail-petrov.me>
2024-08-19 22:09:24 +0300

    update

--- @meets/meet-17/source/main.py
+++ @meets/meet-17/source/main.py
@@ -58,6 +58,57 @@ def serial_number_print(serial_number):
     # odd   -> [1,3,5,7]
     # even  -> [2,4,6,8]
     # total -> [3, 7, 11, 15]
+    
+
+    # Първи вариант за обхождане на масиви / списъци
+    # Знаем че двата списъка са с еднаква дължина. 
+    # да достъпваме елементите в списъка
+    index = 0 
+    total_number_collection = []
+
+    while index < len(odd_number_collection):
+        # при вяско едно преминаване - ще ползвам итератора за да взема елементите на масивите
+        odd_element  = odd_number_collection[index] # N елемент
+        even_element = even_number_collection[index] # N елемент
+        total_number_collection.append(odd_element + even_element)
+        index += 1
+
+    print(total_number_collection)
+
+    # Първи вариант за обхождане на масиви / списъци
+    # Ползваме FOR цикъл
+
+    index = 0
+    total_number_collection = []
+    for element in odd_number_collection:
+
+        odd_element  = odd_number_collection[index] # N елемент
+        even_element = even_number_collection[index] # N елемент
+        total_number_collection.append(odd_element + even_element)
+        index += 1
+    
+    print(total_number_collection)
+
+    # Трети вариант за обхождане на списъци, 
+    #  когато те са повече от един
+    total_number_collection = []
+
+    for (odd_element, even_element) in zip(odd_number_collection, even_number_collection):
+        total_number_collection.append(odd_element + even_element)
+    
+    print(total_number_collection)
+
+
+    # index_list = [0,1,2,3,4,5,6,7,8,9]
+    index_list = range(len(odd_number_collection))
+    total_number_collection = []
+    for index in index_list:
+        odd_element  = odd_number_collection[index] # N елемент
+        even_element = even_number_collection[index] # N елемент
+        total_number_collection.append(odd_element + even_element)
+
+    print(total_number_collection)
+
 
 
 serial_number_print(serial_number)
