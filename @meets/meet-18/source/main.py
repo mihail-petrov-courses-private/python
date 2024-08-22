ae41a2a3dfcfda6c55b819e8645499b1b2540c9b
Mihail Petrov <mail@mihail-petrov.me>
2024-08-22 18:39:36 +0300

    update

--- @meets/meet-18/source/main.py
+++ @meets/meet-18/source/main.py
@@ -30,8 +30,6 @@ character_map = {
     "z" :	"#$",
 }
 
-print(character_map["a"])
-
 # Взимаме думата
 # intervenzia
 # И за всеки нейн символ - който можем да вземем
@@ -39,9 +37,55 @@ print(character_map["a"])
 # За всеки символ - правим проверка в РЕЧНИКА
 # и добавяме резултата в един краен НИЗ
 
-def translateWord(word):
-    pass
+def translate_word(word):
+
+    translated_word = ""
+    for letter in word:
+        translated_letter = character_map[letter]
+        translated_word += translated_letter
+
+    return translated_word
+        
+
+print(translate_word("intervenzia"))
+print(translate_word("korekzia"))
+print(translate_word("pregovori"))
+
+# Да се напише функция която получава някакъв текст. 
+# KJHGVSFJGVDJKFHGDJGFKJDGJFGDJKFGHDGFJHDGFKHDG
+# Искаме да преброим колко пъти се среща всеки един от тези символи в думата
+
+# Помислете как можем да го решим - като знаем че в един речник, можем да имаме
+# само един УНИКАЛЕН ключ
+
+def count_by_letter(word):
+    
+    letter_count_map = {}
+
+    for letter in word:
+
+        if letter_count_map.get(letter) == None:
+            # Трябва да добавя ключ към речника
+           letter_count_map[letter] = 1
+        else:
+           letter_count_map[letter] += 1   
+
+    for key in letter_count_map:
+        print(f"{key} -> {letter_count_map[key]}")
+
+    
+count_by_letter("KJHGVSFJGVDJKFHGDJGFKJDGJFGDJKFGHDGFJHDGFKHDG")
 
 
-# korekzia
-# pregovori
+# print("A".isupper())
+# print("a".isupper())
+
+# Превод на главни букви
+def translate_word_capital(word):
+
+    translated_word = ""
+    for letter in word:
+        translated_letter = character_map[letter]
+        translated_word += translated_letter
+
+    return translated_word
