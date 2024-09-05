9b6d44a1df8dccbfed31a1600c6a34d5c89fa68f
Mihail Petrov <mail@mihail-petrov.me>
2024-09-05 19:55:01 +0300

    update meet_20 source

--- @meets/meet-20/source/application.py
+++ @meets/meet-20/source/application.py
@@ -16,6 +16,15 @@ class Visitor:
         self.name = visitor_name
         self.is_vip = is_visitor_vip
 
+    def get_info(self):
+
+        user_status = "REGULAR"
+        if self.is_vip:
+            user_status = "VIP"
+
+        return f"{self.name} {self.age} {user_status}"
+
+
 visitor = Visitor(
     visitor_name="Mihail",
     visitor_age=10,
@@ -24,9 +33,13 @@ visitor = Visitor(
 # това не е готино
 # visitor.name = "Dragan"
 
-
 class Event:
     title: str
+    visitor_collection: [Visitor] = []
+
+    # Разширете конструктора, така че да приема допълнително поле
+    # дали партито е за пълнолетни потребители. 
+    # НЕ ПЪЛНОЛЕТНИТЕ НЕ МОГАТ да се регистрират.
 
     def __init__(self, event_title):
         self.title = event_title
@@ -35,6 +48,24 @@ class Event:
     # тази функция ще получава ОБЕКТ, от тип Visitor
     # ще добавя този обект в СПИСЪК - поле в класа Event
 
+    def register_visitor(self, visitor_object: Visitor):
+        self.visitor_collection.append(visitor_object)
+
+    # Да направим функция list_all_register_visitors,
+    # която да преминава през всички регистрирани
+    # посетители и да извежда информация за тях
+    # ИМЕ - ВЪЗРАСТ и дали са VIP
+    def list_all_register_visitors(self):
+        for element in self.visitor_collection:
+            print(element.get_info())
+
+
+new_event = Event("Black Party")
+new_event.register_visitor(visitor)
+new_event.register_visitor(visitor)
+new_event.register_visitor(visitor)
+new_event.register_visitor(visitor)
+new_event.list_all_register_visitors()
 
 # Системата трябва да може да създава СЪБИТИЯ, за всяко
 # СЪБИТИЕ имаме посетител. Дадено съитие може да се
